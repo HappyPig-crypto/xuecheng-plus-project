9d4e236943e8681e33632e930c22c02be15b7cce
HappyPig-crypto <2016658893@qq.com>
2023-11-29 22:16:26 +0800

    新增网关服务等功能

--- xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/DictionaryController.java
+++ xuecheng-plus-system/xuecheng-plus-system-api/src/main/java/com/xuecheng/system/controller/DictionaryController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,6 +20,7 @@ import java.util.List;
  */
 @Slf4j
 @RestController
+@RequestMapping("/system")
 public class DictionaryController  {
 
     @Autowired
